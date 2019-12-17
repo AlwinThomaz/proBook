77b98d1c2fb31c3a1ad604ac7f679911bfcc54cb
Alwin Thomas <alwinthomas@MacBook-Pro.local>
2019-12-17 16:09:13 +0000

    Initial commit

---
+++ src/main/java/com/project/probook/ProBookApplication.java
@@ -0,0 +1,13 @@
+package com.project.probook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/probook/ProBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.probook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
