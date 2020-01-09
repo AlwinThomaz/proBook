627993e3ce57dcd41a61ee8a5fd44ba24dc174f7
alwinthomas <alwinthomasgt@outlook.com>
2020-01-09 13:39:04 +0000

    fixed id issues

--- src/main/resources/static/proBook-App/scripts/scripts2.js
+++ src/main/resources/static/proBook-App/scripts/scripts2.js
@@ -16,6 +16,7 @@ function createType() {
    
 
 function createBookmark() {
+    
     let bookmarkName = document.getElementById('bookmarkName').value;
     let bookmarkUrl = document.getElementById('bookmarkUrl').value;
     let bookmarkDescription = document.getElementById('bookmarkDescription').value;
@@ -34,10 +35,27 @@ function createBookmark() {
     }
 )}
 
+function createBookmarkTest() {
+    
+    let bookmarkName = document.getElementById('bookmarkName').value;
+    let bookmarkUrl = document.getElementById('bookmarkUrl').value;
+    let bookmarkDescription = document.getElementById('bookmarkDescription').value;
+    const data = {
+        "name": bookmarkName,
+        "description": bookmarkDescription,
+        "url": bookmarkUrl
+    }
+    axios.post('http://localhost:8080/bookmark/createBookmark', data)
+    .then (console.log(data))
+    .catch ((error) => {
+        console.error(error);
+    }
+)}
+
 
 
 function addBookmarkToType() {
-    debugger
+    
     
     console.log("££££££££££££ " + bookmarkTypeId)
     const data = {
