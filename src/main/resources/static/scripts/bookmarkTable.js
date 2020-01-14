633246d56f573d8a0ff044db3cbe8be5746abbfa
alwinthomas <alwinthomasgt@outlook.com>
2020-01-14 16:56:19 +0000

    tidying up and deleting un-used functions and comments

--- src/main/resources/static/scripts/bookmarkTable.js
+++ src/main/resources/static/scripts/bookmarkTable.js
@@ -5,8 +5,7 @@ const tableBody = document.getElementById("bookmarkTableBody");
 const tableContainer = document.getElementById("bookmarkTableContainer");
 
 function addBookmarkToTable(newEntry, aRow) {
-    // let bookmarkId = document.createElement('td');
-    // bookmarkId.innerHTML = newEntry.bookmarkId;
+
     let bookmarkName = document.createElement('td');
     bookmarkName.innerHTML = newEntry.name;
     bookmarkName.setAttribute("class", "data");
@@ -24,7 +23,7 @@ function addBookmarkToTable(newEntry, aRow) {
 
     let buttonCell = document.createElement('td');
 
-    let  editButton = document.createElement('button');
+    let editButton = document.createElement('button');
     editButton.setAttribute("type", "button");
     editButton.setAttribute("class", "edit");
     editButton.setAttribute("data-toggle", "modal");
@@ -32,7 +31,7 @@ function addBookmarkToTable(newEntry, aRow) {
     editButton.innerText = "Edit";
     buttonCell.appendChild(editButton);
 
-    let  saveButton = document.createElement('button');
+    let saveButton = document.createElement('button');
     saveButton.setAttribute("type", "button");
     saveButton.setAttribute("class", "save");
     saveButton.setAttribute("data-toggle", "modal");
@@ -42,30 +41,19 @@ function addBookmarkToTable(newEntry, aRow) {
     console.log(saveButton.id);
     buttonCell.appendChild(saveButton);
 
-    let  deleteButton = document.createElement('button');
-    deleteButton.setAttribute("type","button");
-    deleteButton.setAttribute("class","delete");
+    let deleteButton = document.createElement('button');
+    deleteButton.setAttribute("type", "button");
+    deleteButton.setAttribute("class", "delete");
     deleteButton.id = newEntry.id;
 
     deleteButton.innerText = "Delete";
     buttonCell.appendChild(deleteButton);
-
-    //button.innnerHTML = <button type="button" class="save" data-toggle="modal" data-target="#exampleModal"> Save</button> <button type="button" class="edit" data-toggle="modal" data-target="#exampleModal"> Edit</button> <button type="button" id="${newEntry.id}" class="delete"> Delete</button >
-    // let deleteButton = document.createElement('td');
-    // deleteButton.innerHTML = `<button type="button" id="${newEntry.id}" class="delete"> Delete</button >`;
-    // let editButton = document.createElement('td');
-    // editButton.innerHTML = `<button type="button" class="edit" data-toggle="modal" data-target="#exampleModal"> Edit</button >`;
-    // let saveButton = document.createElement('td');
-    // saveButton.innerHTML = `<button type="button" class="save" data-toggle="modal" data-target="#exampleModal"> Save</button >`;
 
 
     aRow.appendChild(bookmarkName);
     aRow.appendChild(bookmarkDescription);
     aRow.appendChild(bookmarkUrl);
     aRow.appendChild(buttonCell);
-    // aRow.appendChild(editButton);
-    // aRow.appendChild(deleteButton);
-    // aRow.appendChild(saveButton);
 }
 
 
@@ -88,52 +76,26 @@ function readAllBookmarks() {
 
 }
 
-// //constructor
-// function bookmarkMaker(name, description, url) {
-//     const bookmark = {
-//         bookmarkName: name.value,
-//         bookmarkDescription: description.value,
-//         bookmarkName: url.value,
-//     };
-//     return bookmark;
-// }
-
-// function updateBookmark(bookmarkId) {
-//     let bookmark = bookmarkMaker(updateBookmarkName, updateBookmarkDescription, updateBookmarkUrl);
-//     console.log(bookmark)
-//     //findBookmarkId();
-//     axios.put("localhost:8080/bookmark/updateBookmark?id=" + bookmarkId)
-//         .then((response) => {
-//             console.log(response)
-//             console.log(id)
-//             console.log(bookmark)
-//             console.log(bookmarkName)
-//             readAllBookmarks()
-
-//         }).catch((error) => { console.log(error.message) }).then(() => readAllBookmarks());
-// }
-
 function updateBookmark(bookmarkId) {
     let bookmarkName = document.getElementById('bookmarkName').innerText;
     let bookmarkUrl = document.getElementById('bookmarkUrl').innerText;
     let bookmarkDescription = document.getElementById('bookmarkDescription').innerText;
-    
+
     const data = {
         "name": bookmarkName,
         "description": bookmarkDescription,
         "url": bookmarkUrl
     }
     axios.put("http://localhost:8080/bookmark/updateBookmark?id=" + bookmarkId, data)
-    .then((response) => {
-        console.log(response)
-    }).catch((error) => { console.log(error.message) });
+        .then((response) => {
+            console.log(response)
+        }).catch((error) => { console.log(error.message) });
 }
 
 
 
 function deleteBookmark(bookmarkId) {
     console.log(bookmarkId);
-    // findBookmarkId();
     axios.delete("http://localhost:8080/bookmark/deleteBookmark/" + bookmarkId)
         .then((response) => {
             location.reload();
@@ -145,16 +107,6 @@ function deleteBookmark(bookmarkId) {
         });
 
 }
-
-function findBookmarkId() {
-    let bookmarkId = this.id;
-    return bookmarkId;
-}
-
-// function findDeleteId() {
-//     let deleteId = document.getElementById('deleteId');
-//     return deleteId
-// }
 
 function clickable() {
     $(document).on('click', '.edit', function () {
@@ -189,3 +141,4 @@ function clickable() {
     });
 
 }
+
