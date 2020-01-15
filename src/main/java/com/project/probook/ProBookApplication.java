938d45f80b954001c4a90824017927d2c046ed7f
alwinthomas <alwinthomasgt@outlook.com>
2020-01-15 16:37:25 +0000

    made it deployable and allowed for merging

--- src/main/java/com/project/probook/ProBookApplication.java
+++ src/main/java/com/project/probook/ProBookApplication.java
@@ -2,9 +2,10 @@ package com.project.probook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProBookApplication {
+public class ProBookApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProBookApplication.class, args);
