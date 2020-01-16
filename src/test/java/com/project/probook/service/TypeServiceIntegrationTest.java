9ac6934fd1fde87800ad52ce20f54da70eeb28c4
alwinthomas <alwinthomasgt@outlook.com>
2020-01-16 14:36:12 +0000

    v2

--- src/test/java/com/project/probook/service/TypeServiceIntegrationTest.java
+++ src/test/java/com/project/probook/service/TypeServiceIntegrationTest.java
@@ -3,7 +3,6 @@ package com.project.probook.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.assertj.core.util.Arrays;
